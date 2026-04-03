637f0b5e557706f3316721bb2224fba3e97882ef
FrankkAntonio <frankkantonio69@gmail.com>
2026-04-03 11:44:45 -0300

    feito_CRUD_Professor

--- api/src/main/java/br/com/alunoonline/api/service/ProfessorService.java
+++ api/src/main/java/br/com/alunoonline/api/service/ProfessorService.java
@@ -5,6 +5,9 @@ import br.com.alunoonline.api.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProfessorService {
     @Autowired
@@ -12,5 +15,22 @@ public class ProfessorService {
 
     public void criarProfessor(Professor professor) {
         professorRepository.save(professor);
+    }
+
+    public List<Professor> listarTodosProfessores() {
+        return professorRepository.findAll();
+    }
+
+    public Optional<Professor>  buscarProfessorPorId(Long id) {
+        return professorRepository.findById(id);
+    }
+
+    public void deletarProfessorPorId(Long id) {
+        professorRepository.deleteById(id);
+    }
+
+    public void atualizarProfessorPorId(Long id, Professor professorEditado){
+        professorEditado.setId(id);
+        professorRepository.save(professorEditado);
     }
 }
