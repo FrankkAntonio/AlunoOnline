b18f982739f479df436b879d138d11de1cdaf130
FrankkAntonio <frankkantonio69@gmail.com>
2026-04-06 22:05:57 -0300

    Resolvido

--- api/src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
+++ api/src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
@@ -6,12 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-<<<<<<< HEAD
+
 import java.util.List;
 import java.util.Optional;
 
-=======
->>>>>>> 25177471b13ca1908837cb4f81c90a4fc4706492
+
 @RestController
 @RequestMapping("/professores")
 public class ProfessorController {
@@ -23,7 +22,7 @@ public class ProfessorController {
     public void criarProfessor(@RequestBody Professor professor) {
         professorService.criarProfessor(professor);
     }
-<<<<<<< HEAD
+
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -50,6 +49,4 @@ public class ProfessorController {
     }
 
 
-=======
->>>>>>> 25177471b13ca1908837cb4f81c90a4fc4706492
 }
--- api/src/main/java/br/com/alunoonline/api/service/ProfessorService.java
+++ api/src/main/java/br/com/alunoonline/api/service/ProfessorService.java
@@ -5,12 +5,11 @@ import br.com.alunoonline.api.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
+
 import java.util.List;
 import java.util.Optional;
 
-=======
->>>>>>> 25177471b13ca1908837cb4f81c90a4fc4706492
+
 @Service
 public class ProfessorService {
     @Autowired
@@ -19,7 +18,6 @@ public class ProfessorService {
     public void criarProfessor(Professor professor) {
         professorRepository.save(professor);
     }
-<<<<<<< HEAD
 
     public List<Professor> listarTodosProfessores() {
         return professorRepository.findAll();
@@ -37,6 +35,5 @@ public class ProfessorService {
         professorEditado.setId(id);
         professorRepository.save(professorEditado);
     }
-=======
->>>>>>> 25177471b13ca1908837cb4f81c90a4fc4706492
+
 }
